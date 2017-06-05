ec4c3a4a8d7e3a09cfa359465fde8e0cf55f7506
Demian Licht <demianlicht@hotmail.com>
2017-06-05 18:05:00 +0000

    Fix three images, change bucket env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
     s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
+      :bucket => ENV['S3_BUCKET_NAME'],
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
