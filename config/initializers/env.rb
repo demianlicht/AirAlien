a88c148c8b55550b5df278dabd1aaff68e38b820
Demian Licht <demianlicht@hotmail.com>
2017-06-07 01:53:39 +0000

    Delete

--- config/initializers/env.rb
+++
@@ -1,6 +0,0 @@
-# /config/initializers/env.rb
-
-ENV['S3_BUCKET_NAME'] = 'micasatucasaimages'
-ENV['AWS_SECRET_ACCESS_KEY'] = 'mwqb7nLsf9edBDR5BOI6U/vOSJYj6SwOSgrEE9nc'
-ENV['AWS_ACCESS_KEY_ID'] = 'AKIAJMQZWIORN2VNMZCA'
-ENV['AWS_REGION'] = 'US West (N. California)'
